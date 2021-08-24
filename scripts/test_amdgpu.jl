d34eff54d9ff767509451cb69543a64327224644
Ludovic Räss <ludovic.rass@gmail.com>
2021-08-24 12:19:46 +0200

    Add initial content

---
+++ scripts/test_amdgpu.jl
@@ -0,0 +1,28 @@
+using AMDGPU, Test
+
+function vadd!(c, a, b)
+    i = workitemIdx().x
+    c[i] = a[i] + b[i]
+    return
+end
+
+function test_amdgpu()
+    # cpu version
+    N     = 32
+    a     = rand(Float64, N)
+    b     = rand(Float64, N)
+    c_cpu = a + b
+    # AMD gpu version
+    a_d   = ROCArray(a)
+    b_d   = ROCArray(b)
+    c_d   = similar(a_d)
+    # run gpu kernel
+    wait(@roc groupsize=N vadd!(c_d, a_d, b_d))
+    # gather
+    c     = Array(c_d)
+    # test
+    @test isapprox(c, c_cpu)
+    return
+end
+
+test_amdgpu()
